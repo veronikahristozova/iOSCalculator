134cc10ab901af28da4e3ac95555b0c2bb02933d
Veronika Hristozova <veronikahristozova@192.168.0.106>
2016-10-03 18:37:36 +0300

    first commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,98 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Veronika Hristozova on 10/3/16.
+//  Copyright © 2016 Veronika Hristozova. All rights reserved.
+//
+
+import Foundation
+
+func multiply(op1 : Double, op2 : Double) -> Double {
+    return op1*op2
+}
+
+enum Operation {
+    case Constant(Double)
+    case UnaryOperation((Double)->Double)
+    case BinaryOperation((Double, Double) -> Double)
+    case Equals
+    case Clear
+}
+
+struct PendingBinaryOperationInfo {
+    var binaryFunction: (Double, Double) -> Double
+    var firstOperand: Double
+}
+
+class CalculatorBrain
+{
+    typealias PropertyList = AnyObject
+    private var pending: PendingBinaryOperationInfo?
+    
+    var accumulator: Double = 0
+    var result: Double { get { return accumulator } }
+    var internalProgram = [AnyObject]()
+    var program: PropertyList {
+        get { return internalProgram as CalculatorBrain.PropertyList }
+        set {
+            accumulator = 0
+            pending = nil
+            internalProgram.removeAll()
+            
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand: operand)
+                    } else if let operation = op as? String {
+                        performOperation(symbol: operation)
+                    }
+                }
+            }
+        }
+    }
+    var operations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "×" : Operation.BinaryOperation({ $0*$1 }),
+        "÷" : Operation.BinaryOperation({ $0/$1 }),
+        "+" : Operation.BinaryOperation({ $0+$1 }),
+        "−" : Operation.BinaryOperation({ $0-$1 }),
+        "=" : Operation.Equals,
+        "C" : Operation.Clear
+    ]
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+        internalProgram.append(operand as AnyObject)
+    }
+    
+    func performOperation(symbol: String) {
+        internalProgram.append(symbol as AnyObject)
+        if let operation = operations[symbol] {
+            switch operation  {
+            case .Constant(let value) :
+                accumulator = value
+            case .UnaryOperation(let function) : accumulator = function(accumulator)
+            case .BinaryOperation(let function) :
+                executingPendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals : executingPendingBinaryOperation()
+            case .Clear : accumulator = 0
+            }
+        }
+    }
+    
+    private func executingPendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    func Truncate(acc: Double){
+        
+    }
+    
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Veronika Hristozova on 10/3/16.
+//  Copyright © 2016 Veronika Hristozova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let currentlyInDislay = display.text!
+            display.text = currentlyInDislay + digit
+        } else {
+            display!.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    var displayValue : Double  {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            let a = newValue.truncatingRemainder(dividingBy: 1) == 0 ? String(Int(newValue)) : String(newValue)
+            
+            display.text = a
+        }
+    }
+    
+    var savedProgram: CalculatorBrain.PropertyList?
+    
+    @IBAction func save() {
+        savedProgram = brain.program
+    }
+   
+    @IBAction func restore() {
+        if savedProgram != nil {
+            brain.program = savedProgram!
+            displayValue = brain.result
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+        }
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(symbol: mathematicalSymbol)
+        }
+        displayValue = brain.result
+        
+    }
+}
